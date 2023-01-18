158550cd483917eeea6b7a773082b22f9534b37e
Mike Simons <msimons@etsy.com>
2023-01-18 12:37:22 -0500

    initial commit

---
+++ Targets/TuistBugKit/Tests/TuistBugKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class TuistBugKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("TuistBugKit", "TuistBugKit")
+    }
+}
---
+++ Targets/TuistBugUI/Tests/TuistBugUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class TuistBugUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("TuistBugUI", "TuistBugUI")
+    }
+}
